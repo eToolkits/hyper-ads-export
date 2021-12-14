9b92bbba8549b58199ccc32486f2c5a112f295f4
hungnguyen_2k <hungnm@monsterstudio.io>
2021-12-14 18:22:15 +0700

    update

--- src/Router/PrivateRouter/index.jsx
+++ src/Router/PrivateRouter/index.jsx
@@ -11,51 +11,39 @@ import ChangeAssetsPage from "../../pages/EditGame/ChangeAssets";
 import ExportPage from "../../pages/Export";
 import SelectIdeaPage from "../../pages/EditGame/SelectIdea";
 const PrivateRouter = (props) => {
-	const location = useLocation();
-	const navigate = useNavigate();
-	return (
-		<Box m="30px" w="100%">
-			{location.pathname === "/" ? (
-				""
-			) : (
-				<Button
-					mb="30px"
-					leftIcon={<ArrowLeft color="currentColor" />}
-					onClick={() => navigate(-1)}
-				>
-					Back
-				</Button>
-			)}
-			<Routes>
-				<Route exact path="/" element={<HomePage />} />
-				<Route
-					exact
-					path="/editgame/:idgame"
-					element={<SelectIdeaPage />}
-				/>
-				<Route
-					exact
-					path="/editgame/:idgame/:ididea"
-					element={<EditGame />}
-				/>
-				<Route
-					exact
-					path="/editgame/:idgame/:ididea/changeassets"
-					element={<ChangeAssetsPage />}
-				/>
-				<Route
-					exact
-					path="/editgame/:idgame/:ididea/changesounds"
-					element={<ChangeSoundsPage />}
-				/>
-				<Route
-					exact
-					path="/export/:idgame/:ididea"
-					element={<ExportPage />}
-				/>
-			</Routes>
-		</Box>
-	);
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <Box m="30px" w="100%">
+      {location.pathname === "/" ? (
+        ""
+      ) : (
+        <Button
+          mb="30px"
+          leftIcon={<ArrowLeft color="currentColor" />}
+          onClick={() => navigate(-1)}
+        >
+          Back
+        </Button>
+      )}
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/editgame/:idgame" element={<SelectIdeaPage />} />
+        <Route exact path="/editgame/:idgame/:ididea" element={<EditGame />} />
+        <Route
+          exact
+          path="/editgame/:idgame/:ididea/changeassets"
+          element={<ChangeAssetsPage />}
+        />
+        <Route
+          exact
+          path="/editgame/:idgame/:ididea/changesounds"
+          element={<ChangeSoundsPage />}
+        />
+        <Route exact path="/export/:idgame/:ididea" element={<ExportPage />} />
+      </Routes>
+    </Box>
+  );
 };
 
 export default PrivateRouter;
--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,19 +3,16 @@ import { Player, background } from "@lottiefiles/react-lottie-player";
 import styled from "styled-components";
 
 const LoadingStyle = styled.div`
-    width: 100vw;
-    height: 100vh;
-    position: fixed;
-    z-index:1000
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const Loading = () => {
   return (
     <LoadingStyle>
       <Player
-        src="https://assets3.lottiefiles.com/packages/lf20_wdqlqkhq.json"
+        src="https://assets3.lottiefiles.com/packages/lf20_cud2yjlq.json"
         background="transparent"
         speed="1"
         style={{ width: "150px", height: "150px" }}
