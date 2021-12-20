3733f0c27a423457d7262602a3e7d14d7eee575a
hungnguyen_2k <hungnm@monsterstudio.io>
2021-12-20 18:20:53 +0700

    add auto save asset

--- src/container/ChangeAssets/index.jsx
+++ src/container/ChangeAssets/index.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
 import {
   Image,
   Button,
@@ -12,75 +12,61 @@ import {
   Td,
   Box,
   Flex,
-} from "@chakra-ui/react";
-import { ExportSquare, AudioSquare } from "iconsax-react";
-import DragDrop from "../../components/DragDrop";
-import { DataProcses } from "./../../Utils";
-import Loading from "../../components/Loading";
+  useToast,
+} from '@chakra-ui/react';
+import { ExportSquare, AudioSquare } from 'iconsax-react';
+import DragDrop from '../../components/DragDrop';
+import { DataProcses } from './../../Utils';
+import Loading from '../../components/Loading';
 
-const fs = window.require("fs");
-const TempFolder = "./src/TempCombine";
+const fs = window.require('fs');
+const TempFolder = './src/TempCombine';
 
 const ChangeAssetsContainer = (props) => {
   const { selectedGame } = props;
+  const navigate = useNavigate();
+  const toast = useToast();
   const params = useParams();
   const idgame = params.idgame;
   const ididea = params.ididea;
-  console.log("ChangeAssetsContainer loaded");
+  console.log('ChangeAssetsContainer loaded');
 
   const [variableListState, setVariableListState] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [clearFile, setClearFile] = useState(true);
 
   const handleFile = (data) => {
-    console.dir(data);
-    var imageAsBase64 = fs.readFileSync(data.file.path, "base64");
-    console.log(
-      "🚀 ~ file: index.jsx ~ line 27 ~ handleFile ~ resultB64",
-      imageAsBase64
-    );
+    var imageAsBase64 = fs.readFileSync(data.file.path, 'base64');
     setVariableListState((pre) => {
       pre[data.index].url = `data:image/png;base64,${imageAsBase64}`;
       return pre;
     });
-    console.log(variableListState);
-    let ImageFile = fs
-      .readdirSync(TempFolder)
-      .filter((item) => item.toLowerCase().includes("image"));
-    console.log(ImageFile);
-    // if (ImageFile.length > 0) {
-    //     let content = fs.readFileSync(`${TempFolder}/${ImageFile[0]}`);
-    // } else {
-    // let content = fs.readFileSync(`${TempFolder}/${ImageFile[0]}`);
-    // fs.writeFile(`${TempFolder}/Image.js`, `${content}`, function (err) {
-    //     if (err) return console.log(err);
-    // });
-    // }
+    handleSaveFile();
   };
   const handleSaveFile = () => {
-    fs.writeFileSync(
-      `${TempFolder}/Image.js`,
-      variableListState
-        .map((item) => `var ${item.name} = "${item.url}";`)
-        .join("\n"),
-      function (err) {
-        throw err;
-      }
-    );
-  };
-  // clear old file temp when load component
-  useEffect(() => {
-    fs.readdirSync(TempFolder)
-      .filter((item) => item.toLowerCase().includes("image"))
-      .forEach((file) => {
-        console.log(file);
-        fs.unlink(`./src/TempCombine/${file}`, (err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-        });
+    try {
+      fs.writeFileSync(
+        `${TempFolder}/Image.js`,
+        variableListState
+          .map((item) => `var ${item.name} = "${item.url}";`)
+          .join('\n')
+      );
+      toast({
+        title: `Save Asset succeslully!`,
+        position: 'top',
+        isClosable: true,
+        status: 'success',
       });
-  }, []);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //save file  before change route
+  const handleChangePage = (route) => {
+    handleSaveFile();
+    navigate(`${route}`);
+  };
 
   //transform data to render
   useEffect(() => {
@@ -91,22 +77,22 @@ const ChangeAssetsContainer = (props) => {
 
     const ImageFile = fs
       .readdirSync(`${ideaSelect.linkBaseCode}`)
-      .filter((item) => item.toLowerCase().includes("image"));
+      .filter((item) => item.toLowerCase().includes('image'));
 
     const contentImageFile = fs.readFileSync(
       `${ideaSelect.linkBaseCode}/${ImageFile[0]}`,
-      "utf8"
+      'utf8'
     );
 
     const variableList = contentImageFile
-      .split("var ")
+      .split('var ')
       .map((item) => {
         return item
-          .split(" = ")
+          .split(' = ')
           .map((item, index) =>
             index == 1
-              ? item.trim().replace(/["]/g, "").slice(0, -1)
-              : item.trim().replace(/["]/g, "")
+              ? item.trim().replace(/["]/g, '').slice(0, -1)
+              : item.trim().replace(/["]/g, '')
           );
       })
       .slice(1);
@@ -114,8 +100,8 @@ const ChangeAssetsContainer = (props) => {
     //optimize performance
     let dataTranformStateTemp = [];
     variableList.map((item, index) => {
-      const img = document.createElement("img");
-      img.setAttribute("src", item[1]);
+      const img = document.createElement('img');
+      img.setAttribute('src', item[1]);
       img.onload = function () {
         var w = img.width;
         var h = img.height;
@@ -125,7 +111,6 @@ const ChangeAssetsContainer = (props) => {
           width: w,
           height: h,
         });
-        console.log("render");
         if (index == variableList.length - 1) {
           setVariableListState((pre) => [...dataTranformStateTemp]);
           const time = setTimeout(() => {
@@ -136,7 +121,25 @@ const ChangeAssetsContainer = (props) => {
         }
       };
     });
-    console.log(variableListState);
+  }, []);
+
+  // clear old file temp when load component
+  useEffect(() => {
+    if (clearFile) {
+      fs.readdirSync(TempFolder)
+        .filter((item) => item.toLowerCase().includes('image'))
+        .forEach((file) => {
+          console.log(file);
+          fs.unlink(`./src/TempCombine/${file}`, (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          });
+        });
+      setClearFile(false);
+    }
+    handleSaveFile()
   }, []);
 
   return (
@@ -145,78 +148,74 @@ const ChangeAssetsContainer = (props) => {
         <Loading />
       ) : (
         <Box mb="5">
-          <Table w="100%" my="5" variant="striped" colorScheme="gray">
-            <Thead>
-              <Tr>
-                <Th textAlign="center">Serial</Th>
-                <Th>Name Asset</Th>
-                <Th>Current Asset</Th>
-                <Th>Current Size</Th>
-                <Th>New Asset</Th>
-                <Th>New Size</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {variableListState.map((item, index) => {
-                return (
-                  <Tr key={index}>
-                    <Td textAlign="center">{index + 1}</Td>
-                    <Td>{item.name.slice(3, -3)}</Td>
-                    <Td>g
-                      <Image
-                        boxSize="150px"
-                        objectFit="contain"
-                        src={item.url}
-                        alt="Error Image"
-                        _hover={{
-                          transform: "scale(1.5)",
-                          transition: "all 0.3s",
-                        }}
-                      />
-                    </Td>
-                    <Td>
-                      {item.height}x{item.width}
-                    </Td>
-                    <Td>
-                      <DragDrop
-                        text="asset"
-                        handleFile={handleFile}
-                        indexFile={index}
-                        type="image/jpeg, image/png"
-                      />
-                    </Td>
-                    <Td>null</Td>
-                  </Tr>
-                );
-              })}
-            </Tbody>
-          </Table>
-          <Flex justifyContent="space-between">
+          <Box height="75vh" overflowY="scroll">
+            <Table w="100%" my="5" variant="striped" colorScheme="gray">
+              <Thead>
+                <Tr>
+                  <Th textAlign="center">Index</Th>
+                  <Th>Name Asset</Th>
+                  <Th>Current Asset</Th>
+                  <Th>Current Size</Th>
+                  <Th>New Asset</Th>
+                  <Th>New Size</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {variableListState.map((item, index) => {
+                  return (
+                    <Tr key={index}>
+                      <Td textAlign="center">{index + 1}</Td>
+                      <Td>{item.name.slice(3, -3)}</Td>
+                      <Td>
+                        <Image
+                          loading="lazy"
+                          boxSize="150px"
+                          objectFit="contain"
+                          src={item.url}
+                          alt="Error Image"
+                          _hover={{
+                            transform: 'scale(1.5)',
+                            transition: 'all 0.3s',
+                          }}
+                        />
+                      </Td>
+                      <Td>
+                        {item.height}x{item.width}
+                      </Td>
+                      <Td>
+                        <DragDrop
+                          text="asset"
+                          handleFile={handleFile}
+                          indexFile={index}
+                          type="image/jpeg, image/png"
+                        />
+                      </Td>
+                      <Td>null</Td>
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
+          </Box>
+          <Flex justifyContent="flex-end" mt="5">
             <Box>
-              <Button mb="5" colorScheme="green" onClick={handleSaveFile}>
-                Save
-              </Button>{" "}
-            </Box>
-            <Box>
-              <Link to={`/editgame/` + idgame + "/" + ididea + `/changesounds`}>
-                <Button
-                  mb="5"
-                  colorScheme="green"
-                  rightIcon={<AudioSquare size="20" color="currentColor" />}
-                >
-                  Change Sounds
-                </Button>{" "}
-              </Link>
-              <Link to={"/export/" + idgame + `/` + ididea}>
-                <Button
-                  mb="5"
-                  ml="5"
-                  colorScheme="green"
-                  rightIcon={<ExportSquare size="20" color="currentColor" />}
-                >
-                  Export Now
-                </Button>{" "}
-              </Link>
+              <Button
+                colorScheme="green"
+                rightIcon={<AudioSquare size="20" color="currentColor" />}
+                onClick={() =>
+                  handleChangePage(`/editgame/${idgame}/${ididea}/changesounds`)
+                }
+              >
+                Change Sounds
+              </Button>{' '}
+              <Button
+                ml="5"
+                colorScheme="green"
+                rightIcon={<ExportSquare size="20" color="currentColor" />}
+                onClick={() => handleChangePage(`/export/${idgame}/${ididea}`)}
+              >
+                Export Now
+              </Button>{' '}
             </Box>
           </Flex>
         </Box>
--- src/container/ChangeSounds/index.jsx
+++ src/container/ChangeSounds/index.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
-import PropTypes from "prop-types";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
 import {
-  Flex,
   Button,
   Table,
   Thead,
@@ -11,86 +10,204 @@ import {
   Th,
   Td,
   Box,
-} from "@chakra-ui/react";
-import { ExportSquare, GalleryEdit } from "iconsax-react";
-import DragDrop from "../../components/DragDrop";
-var fs = window.require("fs");
+  Flex,
+  useToast,
+} from '@chakra-ui/react';
+import { ExportSquare, AudioSquare } from 'iconsax-react';
+import DragDrop from '../../components/DragDrop';
+import { DataProcses } from './../../Utils';
+import Loading from '../../components/Loading';
+
+const fs = window.require('fs');
+const TempFolder = './src/TempCombine';
 
 const ChangeSoundsContainer = (props) => {
+  const { selectedGame } = props;
+  const navigate = useNavigate();
+  const toast = useToast();
   const params = useParams();
-  const ididea = params.ididea;
   const idgame = params.idgame;
-  const handleFile = (file) => {
-    console.log(file.path);
-    var imageAsBase64 = fs.readFileSync(file.path, "base64");
+  const ididea = params.ididea;
+  console.log('ChangeSoundsContainer loaded');
+
+  const [variableListState, setVariableListState] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [clearFile, setClearFile] = useState(true);
+
+  const handleFile = (data) => {
+    var soundAsBase64 = fs.readFileSync(data.file.path, 'base64');
     console.log(
-      "🚀 ~ file: index.jsx ~ line 27 ~ handleFile ~ resultB64",
-      imageAsBase64
+      '🚀 ~ file: index.jsx ~ line 142 ~ handleFile ~ soundAsBase64',
+      soundAsBase64
     );
+    setVariableListState((pre) => {
+      pre[data.index].url = `data:audio/mpeg;base64,${soundAsBase64}`;
+      return pre;
+    });
+    handleSaveFile();
   };
+  const handleSaveFile = () => {
+    try {
+      fs.writeFileSync(
+        `${TempFolder}/Sound.js`,
+        variableListState
+          .map((item) => `var ${item.name} = "${item.src}";`)
+          .join('\n')
+      );
+      toast({
+        title: `Save Asset succeslully!`,
+        position: 'top',
+        isClosable: true,
+        status: 'success',
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //save file  before change route
+  const handleChangePage = (route) => {
+    handleSaveFile();
+    navigate(`${route}`);
+  };
+
+  //transform data to render
+  useEffect(() => {
+    const ideaSelect =
+      selectedGame.idea[
+        selectedGame?.idea?.findIndex((idea) => idea.id === ididea)
+      ];
+
+    const SoundFile = fs
+      .readdirSync(`${ideaSelect.linkBaseCode}`)
+      .filter((item) => item.toLowerCase().includes('sound'));
+
+    const contentSoundFile = fs.readFileSync(
+      `${ideaSelect.linkBaseCode}/${SoundFile[0]}`,
+      'utf8'
+    );
+
+    const variableList = contentSoundFile
+      .split('var ')
+      .map((item) => {
+        return item
+          .split(' = ')
+          .map((item, index) =>
+            index == 1
+              ? item.trim().replace(/["]/g, '').slice(0, -1)
+              : item.trim().replace(/["]/g, '')
+          );
+      })
+      .slice(1);
+
+    // //optimize performance
+    let dataTranformStateTemp = [];
+    variableList.map((item, index) => {
+      dataTranformStateTemp.push({
+        name: item[0],
+        src: item[1],
+      });
+      if (index == variableList.length - 1) {
+        setVariableListState((pre) => [...dataTranformStateTemp]);
+        const time = setTimeout(() => {
+          setIsLoading(false);
+          clearTimeout(time);
+        }, 2000);
+        dataTranformStateTemp = [];
+      }
+    });
+  }, []);
+  // clear old file temp when load component
+  useEffect(() => {
+    if (clearFile) {
+      fs.readdirSync(TempFolder)
+        .filter((item) => item.toLowerCase().includes('sound'))
+        .forEach((file) => {
+          console.log(file);
+          fs.unlink(`./src/TempCombine/${file}`, (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          });
+        });
+      setClearFile(false);
+    }
+    handleSaveFile()
+  }, []);
   return (
     <>
-      <Box mb="5">
-        <Table my="5" variant="striped" colorScheme="gray" w="100%">
-          <Thead>
-            <Tr>
-              <Th>Name Sound</Th>
-              <Th>Current Sound</Th>
-              <Th>New Sound</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>inches</Td>
-              <Td>
-                {" "}
-                <audio controls>
-                  <source src="horse.ogg" />
-                </audio>
-              </Td>
-              <Td>
-                <DragDrop
-                  text="audio"
-                  handleFile={handleFile}
-                  type="audio/mp3, audio/mpeg"
-                />
-              </Td>
-            </Tr>
-          </Tbody>
-        </Table>
-        <Flex justifyContent="space-between">
-          <Box>
-            <Button mb="5" colorScheme="green">
-              Save
-            </Button>{" "}
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Box mb="5">
+          <Box height="75vh" overflowY="scroll">
+            <Table w="100%" my="5" variant="striped" colorScheme="gray">
+              <Thead>
+                <Tr>
+                  <Th textAlign="center">Index</Th>
+                  <Th>Name Sound</Th>
+                  <Th>Current Sound</Th>
+                  <Th>New Sound</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {variableListState.map((item, index) => {
+                  return (
+                    <Tr key={index}>
+                      <Td textAlign="center">{index + 1}</Td>
+                      <Td>{item.name.slice(0, -3)}</Td>
+                      <Td>
+                        <audio controls>
+                          <source src={item.src} />
+                        </audio>
+                      </Td>
+                      <Td>
+                        <DragDrop
+                          text="asset"
+                          handleFile={handleFile}
+                          indexFile={index}
+                          type="audio/mp3, audio/mpeg"
+                        />
+                      </Td>
+                    </Tr>
+                  );
+                })}
+              </Tbody>
+            </Table>
           </Box>
-          <Box>
-            <Link to={`/editgame/` + idgame + "/" + ididea + `/changeassets`}>
+          <Flex justifyContent="flex-end" mt="5">
+            <Box>
               <Button
-                mb="5"
                 colorScheme="green"
-                rightIcon={<GalleryEdit color="currentColor" />}
+                rightIcon={<AudioSquare size="20" color="currentColor" />}
+                onClick={() =>
+                  handleChangePage(`/editgame/${idgame}/${ididea}/changeassets`)
+                }
               >
                 Change Assets
-              </Button>{" "}
-            </Link>
-            <Link to={"/export/" + idgame + `/` + ididea}>
+              </Button>{' '}
               <Button
                 ml="5"
-                mb="5"
                 colorScheme="green"
-                rightIcon={<ExportSquare color="currentColor" />}
+                rightIcon={<ExportSquare size="20" color="currentColor" />}
+                onClick={() => handleChangePage(`/export/${idgame}/${ididea}`)}
               >
                 Export Now
-              </Button>{" "}
-            </Link>
-          </Box>
-        </Flex>
-      </Box>
+              </Button>{' '}
+            </Box>
+          </Flex>
+        </Box>
+      )}
     </>
   );
 };
 
 ChangeSoundsContainer.propTypes = {};
 
-export default ChangeSoundsContainer;
+const mapStateToProps = (state) => {
+  return {
+    selectedGame: state.gameSelected,
+  };
+};
+export default connect(mapStateToProps, null)(ChangeSoundsContainer);
